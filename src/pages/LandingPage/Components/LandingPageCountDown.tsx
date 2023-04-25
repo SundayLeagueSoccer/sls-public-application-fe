488e6944612113789c2ea9c26218b2b64cd49e1a
Oghenetega Esedere <arcteggzz@gmail.com>
2023-04-25 16:27:32 +0100

    feat: implemented countdown section

--- src/pages/LandingPage/Components/LandingPageCountDown.tsx
+++ src/pages/LandingPage/Components/LandingPageCountDown.tsx
@@ -1,10 +1,30 @@
 import styles from "../styles/LandingPageCountDown.module.scss";
+import useApp from "../../../hooks/useApp";
 
 const LandingPageCountDown = () => {
+  const { darkMode } = useApp();
   return (
     <>
       <section className={styles.LandingPageCountDown}>
-        LandingPageCountDown
+        <div
+          className={
+            darkMode ? styles.top_container_dark : styles.top_container_light
+          }
+        >
+          <h2>Next Match Day</h2>
+          <div className={styles.figures_container}>
+            <h5>05</h5>
+            <h5>05</h5>
+            <h5>05</h5>
+            <h5>05</h5>
+          </div>
+        </div>
+        <div className={styles.bottom_container}>
+          <h6>Days</h6>
+          <h6>Hours</h6>
+          <h6>Mins</h6>
+          <h6>Secs</h6>
+        </div>
       </section>
     </>
   );
