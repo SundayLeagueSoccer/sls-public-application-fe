52ea3edf8a1f83b91ab3de60d672b55d15044154
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2023-04-30 21:32:16 +0100

    devMine first commit

--- src/pages/AboutPage/Components/AboutMetrics.tsx
+++ src/pages/AboutPage/Components/AboutMetrics.tsx
@@ -1,10 +1,21 @@
 import styles from "../styles/AboutMetrics.module.scss";
 
 const AboutMetrics = () => {
+  const metricsContent = [
+    {number: '100', title: 'matches played', desc: "We’ve played lots of matches in different weathers"},
+    {number: '50', title: 'active players', desc: "We’ve a system wherein players get to play and connect"},
+    {number: '05', title: 'players award', desc: "We’ve awarded several players on their outstanding performance."}
+  ]
   return (
     <>
       <section className={styles.AboutMetrics}>
-        <h1>AboutMetrics</h1>
+      {metricsContent.map(({number, title, desc}, index) => (
+          <div key={index} className={styles.Metrics_NumberBox}>
+            <p>{index !== 1? `${number}+` : number}</p>
+            <p>{title}</p>
+            <p>{desc}</p>
+          </div>
+          ))}
       </section>
     </>
   );
