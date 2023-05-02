2500cd07ef42b987bf479b263634175a1a48e51c
Oghenetega Esedere <arcteggzz@gmail.com>
2023-05-02 11:43:46 +0100

    fix: corrected all empty alt tags for images

--- src/pages/LandingPage/Components/LandingPageCountDown.tsx
+++ src/pages/LandingPage/Components/LandingPageCountDown.tsx
@@ -1,8 +1,44 @@
 import styles from "../styles/LandingPageCountDown.module.scss";
 import useApp from "../../../hooks/useApp";
+import { useEffect, useState } from "react";
 
 const LandingPageCountDown = () => {
   const { darkMode } = useApp();
+  const [daysToMatch, SetDaysToMatch] = useState<string>();
+  const [hoursToMatch, SetHoursToMatch] = useState<string>();
+  const [minutesToMatch, setMinutesToMatch] = useState<string>();
+  const [secondsToMatch, setSecondsToMatch] = useState<string>();
+
+  const countdownTimer = () => {
+    const currentTime = new Date();
+
+    //day of week
+    const dayOfWeek = currentTime.getDay();
+    const daysToSunday = 7 - dayOfWeek;
+    SetDaysToMatch(daysToSunday.toString().padStart(2, "0"));
+
+    //hour of day
+    const hourOfDay = currentTime.getHours();
+    const hoursToFourPm = 15 - hourOfDay;
+    SetHoursToMatch(hoursToFourPm.toString().padStart(2, "0"));
+
+    //hour of minutes
+    const minuteOfDay = currentTime.getMinutes();
+    const minutesToHour = 60 - minuteOfDay;
+    setMinutesToMatch(minutesToHour.toString().padStart(2, "0"));
+
+    //hour of seconds
+    const secondOfDay = currentTime.getSeconds();
+    const secondsToMinute = 60 - secondOfDay;
+    setSecondsToMatch(secondsToMinute.toString().padStart(2, "0"));
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      countdownTimer();
+    }, 1000);
+  }, []);
+
   return (
     <>
       <section className={styles.LandingPageCountDown}>
@@ -13,10 +49,10 @@ const LandingPageCountDown = () => {
         >
           <h2>Next Match Day</h2>
           <div className={styles.figures_container}>
-            <h5>05</h5>
-            <h5>05</h5>
-            <h5>05</h5>
-            <h5>05</h5>
+            <h5>{daysToMatch}</h5>
+            <h5>{hoursToMatch}</h5>
+            <h5>{minutesToMatch}</h5>
+            <h5>{secondsToMatch}</h5>
           </div>
         </div>
         <div className={styles.bottom_container}>
