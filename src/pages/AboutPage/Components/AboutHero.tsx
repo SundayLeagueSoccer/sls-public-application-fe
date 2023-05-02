3a7faf1f6a01d2130407135c6ac6ee3f99a19e26
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2023-05-02 17:06:08 +0100

    devMine first commit

--- src/pages/AboutPage/Components/AboutHero.tsx
+++ src/pages/AboutPage/Components/AboutHero.tsx
@@ -1,10 +1,15 @@
 import styles from "../styles/AboutHero.module.scss";
 
+
 const AboutHero = () => {
+
+ 
   return (
     <>
       <section className={styles.AboutHero}>
-        <h1>AboutHero</h1>
+          <h4 className="">about us</h4>
+          <h1>We do things differently...</h1>
+          <p>we believe that football is not just a game, but a way of life. That's why our league is more than just drills and exercises. They're a way to develop the tactical understanding you need need to dominate the field.</p>            
       </section>
     </>
   );
--- src/pages/AboutPage/Components/AboutMetrics.tsx
+++ src/pages/AboutPage/Components/AboutMetrics.tsx
@@ -1,10 +1,33 @@
 import styles from "../styles/AboutMetrics.module.scss";
+import MetricsDesign from '../images/MetricsDesign.png'
+import MetricsDesignSm from '../images/MetricDesignSm.png'
+import MetricsDesignSmTwo from '../images/MetricDesignSm(2).png'
+import MetricsDesignTwo from '../images/MetricDesign(2).png'
 
 const AboutMetrics = () => {
+  const metricsContent = [
+    {number: '100', title: 'matches played', desc: "We’ve played lots of matches in different weathers"},
+    {number: '50', title: 'active players', desc: "We’ve a system wherein players get to play and connect"},
+    {number: '05', title: 'players award', desc: "We’ve awarded several players on their outstanding performance."}
+  ]
   return (
     <>
       <section className={styles.AboutMetrics}>
-        <h1>AboutMetrics</h1>
+          <div className={
+            styles.Metrics_Container
+          }>
+          {metricsContent.map(({number, title, desc}, index) => (
+        <div key={index} className={styles.Metrics_NumberBox}>
+            <p>{index !== 1? `${number}+` : number}</p>
+            <p>{title}</p>
+            <p>{desc}</p>
+          </div>
+          ))}
+          </div>
+          <img className={styles.Metrics_design_sm} src={MetricsDesignSm} alt="a background design"/>
+          <img className={styles.Metrics_design_sm_two} src={MetricsDesignSmTwo} alt="a background design"/>
+          <img className={styles.Metrics_design} src={MetricsDesign} alt="a background design"/>
+          <img className={styles.Metrics_design_two} src={MetricsDesignTwo} alt="a background design"/>
       </section>
     </>
   );
