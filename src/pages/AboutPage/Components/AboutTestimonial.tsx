479c02625aa12c20134f7d9de11faeaf57f74bfa
Oghenetega Esedere <arcteggzz@gmail.com>
2023-05-06 23:58:16 +0100

    feat: corrected centering on screen and added carousel sliders to project. also added player cards

--- src/pages/AboutPage/Components/AboutTestimonial.tsx
+++ src/pages/AboutPage/Components/AboutTestimonial.tsx
@@ -1,24 +1,12 @@
-import { useState, useEffect } from "react";
 import styles from "../styles/AboutTestimonial.module.scss";
-
-// Image
-import TestimonialImage from "../images/testimonial.png";
-import Arrow from "../images/arrow.png";
-
-// testimonial
-import { testimonials } from "../assets/testimonials";
+import TestimonialCarousel from "./TestimonialCarousel";
 
 const AboutTestimonial = () => {
-  const [testimonies, setTestimonies] = useState(testimonials);
-  useEffect(() => {
-    // console.log(testimony.postion)
-  }, []);
-
   return (
     <>
       <section className={styles.AboutTestimonial}>
-        <div className={styles.AboutTestimonial_Text}>
-          <h4>Testimonial</h4>
+        <div className={styles.left}>
+          <h3>Testimonial</h3>
           <h2>
             <span>SLS</span> Players thoughts
           </h2>
@@ -28,36 +16,9 @@ const AboutTestimonial = () => {
             for the game and embrace every opportunity to play.
           </p>
         </div>
-        {testimonies.map((testimony) => (
-          <div className={styles.AboutTestimonial_Image}>
-            <img src={TestimonialImage} alt="" />
-            {/* Testimony under Image */}
-            <div
-              className={
-                testimony.postion === "DEF"
-                  ? styles.AboutTestimonial_WriteUp_def
-                  : testimony.postion === "AMF"
-                  ? styles.AboutTestimonial_WriteUp_amf
-                  : testimony.postion === "STR"
-                  ? styles.AboutTestimonial_WriteUp_str
-                  : testimony.postion === "GOL"
-                  ? styles.AboutTestimonial_WriteUp_gol
-                  : styles.AboutTestimonial_WriteUp
-              }
-            >
-              <div className={styles.AboutTestimonial_WriteUp_Text}>
-                <p>{testimony.statement}</p>
-              </div>
-              <div className={styles.AboutTestimonial_WriteUp_Author}>
-                <h4>{testimony.playerName}</h4>
-                <div className={styles.AboutTestimonial_Profile}>
-                  <b>View Profile</b>
-                  <img src={Arrow} alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+        <div className={styles.right}>
+          <TestimonialCarousel />
+        </div>
       </section>
     </>
   );
