fef65aae224a4a5dbe49e54d8773375b2f3e7d01
Oghenetega Esedere <arcteggzz@gmail.com>
2023-05-07 05:17:05 +0100

    feat: completed playerCard component

---
+++ src/Components/PlayerInfoCard/svgs/ForwardIcon.tsx
@@ -0,0 +1,27 @@
+type ForwardIconProp = {
+  fillColor: string | undefined;
+};
+
+const ForwardIcon = ({ fillColor }: ForwardIconProp) => {
+  return (
+    <>
+      <svg
+        width="13"
+        height="13"
+        viewBox="0 0 13 13"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.64062 6.84961H12.1406M12.1406 6.84961L6.89062 1.59961M12.1406 6.84961L6.89062 12.0996"
+          stroke={fillColor}
+          stroke-width="1.67"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default ForwardIcon;
