3fb5aab87e724d59aa5fd27f4719e0bb606dd989
Henry <aguhenrychuks@gmail.com>
2023-05-09 22:50:11 +0100

    feat: about whyus updated

--- src/pages/AboutPage/Components/AboutWhyUs.tsx
+++ src/pages/AboutPage/Components/AboutWhyUs.tsx
@@ -7,22 +7,21 @@ const AboutWhyUs = () => {
   return (
     <>
       <section className={styles.AboutWhyUs}>
-      <div className={styles.AboutWhyUs_header}>
-            <h4>Why us?</h4>
-            <h2>
-              <span>SLS</span> Vision and Mission
-            </h2>
-            <p>
-              SLS has got more than 50 active players and is committed to
-              building a successful and sustainable organization that values
-              teamwork, integrity, and excellence both on and off the field.
-              Stay jiggy!!!
-            </p>
-          </div>
-        <div className={styles.AboutWhyUs_image}>
+        <div className={styles.AboutWhyUs_Mobile}>
+          <h4>Why us?</h4>
+          <h2>
+            <span>SLS</span> Vision and Mission
+          </h2>
+          <p>
+            SLS has got more than 50 active players and is committed to building
+            a successful and sustainable organization that values teamwork,
+            integrity, and excellence both on and off the field. Stay jiggy!!!
+          </p>
+        </div>
+        <div className={styles.AboutWhyUs_Left}>
           <img src={whyUsImage} alt="" />
         </div>
-        <div className={styles.AboutWhyUs_text}>
+        <div className={styles.AboutWhyUs_Right}>
           <div className={styles.AboutWhyUs_header}>
             <h4>Why us?</h4>
             <h2>
