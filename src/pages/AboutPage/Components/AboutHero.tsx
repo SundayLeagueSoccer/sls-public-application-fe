7053ea29a4a670b7795c2175b9777f6d6e26f349
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2023-05-13 00:05:40 +0100

    devMine corrections to the AboutPage

--- src/pages/AboutPage/Components/AboutHero.tsx
+++ src/pages/AboutPage/Components/AboutHero.tsx
@@ -7,9 +7,11 @@ const AboutHero = () => {
   return (
     <>
       <section className={styles.AboutHero}>
+          <div className={styles.container}>
           <p className={styles.subheading}>about us</p>
           <h1 className={styles.heading}>We do things differently...</h1>
-          <p className={styles.support_text}>we believe that football is not just a game, but a way of life. That's why our league is more than just drills and exercises. They're a way to develop the tactical understanding you need need to dominate the field.</p>            
+          <p className={styles.support_text}>we believe that football is not just a game, but a way of life. That's why our league is more than just drills and exercises. They're a way to develop the tactical understanding you need need to dominate the field.</p>
+            </div>            
       </section>
     </>
   );
