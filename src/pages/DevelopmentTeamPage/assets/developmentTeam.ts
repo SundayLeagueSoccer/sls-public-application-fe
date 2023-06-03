3f3ea0544448908725a3c83dca6f45ed1b5b4ced
Oghenetega Esedere <arcteggzz@gmail.com>
2023-06-03 12:43:14 +0100

    feat: updated backend API hosting url

--- src/pages/DevelopmentTeamPage/assets/developmentTeam.ts
+++ src/pages/DevelopmentTeamPage/assets/developmentTeam.ts
@@ -20,8 +20,8 @@ export const developmentTeam: developmentTeamType[] = [
     fullName: "Okpani Margaret",
     role: "Product Designer",
     image: "",
-    twitterLink: "",
-    linkedinLink: "",
+    twitterLink: "https://twitter.com/m3garchi",
+    linkedinLink: "https://www.linkedin.com/in/margaretokpani/",
     portfolioLink: "",
   },
   {
@@ -37,5 +37,31 @@ export const developmentTeam: developmentTeamType[] = [
     role: "Frontend Developer",
     image: "",
     githubLink: "https://github.com/OGHENEMINE",
+    twitterLink: "https://twitter.com/Oghorodi_mine",
+    linkedinLink: "https://www.linkedin.com/mwlite/in/oghenemine-oghorodi-444446246",
+  },
+  {
+    fullName: "Bobby Osagie",
+    role: "Product Designer",
+    image: "",
+    twitterLink: "",
+    linkedinLink: "",
+  },
+  {
+    fullName: "Ibiyeye Damilola",
+    role: "Product Designer",
+    image: "",
+    twitterLink: "https://twitter.com/a_ibiyeye",
+    linkedinLink:
+      "https://www.linkedin.com/in/oluwadamilola-ibiyeye-884975247/",
+    portfolioLink:
+      "https://www.figma.com/file/G6WWiDR7q942T3cN9vRnSK/Bootcamp-Task?type=design&node-id=313%3A4964&t=qJOPXk8Lo0m5LYsA-1",
+  },
+  {
+    fullName: "Okesiji Abisola",
+    role: "Product Designer",
+    image: "",
+    twitterLink: "https://twitter.com/taiwookesiji",
+    linkedinLink: "https://www.linkedin.com/in/taiwo-a-okesiji-a4577355",
   },
 ];
